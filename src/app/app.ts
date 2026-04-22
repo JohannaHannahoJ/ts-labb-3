3713d535a5db5aab4c065f216d54ca4937145b22
JohannaHannahoJ <joha2102@student.miun.se>
2026-04-22 14:16:20 +0200

    Move nav-menu to component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { MainNav } from './main-nav/main-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, MainNav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
