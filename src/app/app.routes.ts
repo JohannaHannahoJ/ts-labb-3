87c6947589ae3a885820bac20c71fd52a1108439
JohannaHannahoJ <joha2102@student.miun.se>
2026-04-22 11:31:57 +0200

    Initial setup: routing, pages and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Converter } from './converter/converter';
+import { Index } from './index';
+import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "hem", component: Index }, 
+    { path: "omvandlare", component: Converter }, 
+    { path: "om", component: About }, 
+    { path: "", redirectTo: "hem", pathMatch: "full" }, 
+    { path: "404", component: NotFound }, 
+    { path: "**", redirectTo: "404", pathMatch: "full"}
+];
