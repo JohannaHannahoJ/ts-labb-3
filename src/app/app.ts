5904c8baccecab4b7a7c0b960ef73a7ee100c95f
JohannaHannahoJ <joha2102@student.miun.se>
2026-04-23 20:51:13 +0200

    Move footer into separate component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainNav } from './main-nav/main-nav';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainNav],
+  imports: [RouterOutlet, MainNav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
