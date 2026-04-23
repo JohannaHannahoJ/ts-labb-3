e6fb44ef685272a4e27402351e4cdaaadd10fa1e
JohannaHannahoJ <joha2102@student.miun.se>
2026-04-23 18:43:23 +0200

    Add temperature and length converters

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
-export class Converter {}
+export class Converter {
+  temp: number = 0;
+  result1: string = '';
+  length: number = 1;
+  result2: string = '';
+
+  // metoder
+  meterToFoot(): void {
+    const feet = this.length * 3.28084;
+    this.result1 = `${feet} ft`;
+  }
+
+  footToMeter(): void {
+    const meter = this.length / 3.28084;
+    this.result1 = `${meter} m`;
+  }
+
+  celsiusToFahrenheit(): void {
+    const fahrenheit = (this.temp * 9 / 5) + 32;
+    this.result2 = `${fahrenheit} °F`;
+  }
+
+  fahrenheitToCelsius(): void {
+    const celsius = (this.temp - 32) * 5 / 9;
+    this.result2 = `${celsius} °C`;
+  }
+}
